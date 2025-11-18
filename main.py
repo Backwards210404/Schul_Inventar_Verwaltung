b1a58b5624bac9b4119a67fa8a2d06fabf5c8806
zPinguno <ben.diersch@icloud.com>
2025-11-18 10:54:25 +0100

    init

---
+++ main.py
@@ -0,0 +1,5 @@
+import sys
+
+from user import User
+sys.path.append(".")
+User().firstName
---
+++ user.py
@@ -0,0 +1,34 @@
+class User:
+    firstName = ''
+    lastName = ''
+    userName = ''
+    password = ''
+    role = ''
+
+    def __init__(self,firstName, lastName, userName, password, role):
+        self.firstName = firstName
+        self.lastName = lastName
+        self.userName = userName
+        self.password = password
+        self.role = role
+
+    def getFirstName(self):
+        return self.firstName
+    def setFirstName(self, firstName):
+        self.firstName = firstName
+    def getLastName(self):
+        return self.lastName
+    def setLastName(self, lastName):
+        self.lastName = lastName
+    def getUserName(self):
+        return self.userName
+    def setUserName(self, userName):
+        self.userName = userName
+    def getPassword(self):
+        return self.password
+    def setPassword(self, password):
+        self.password = password
+    def setRole(self, role):
+        self.role = role
+    def getRole(self):
+        return self.role
