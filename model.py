e9a7b8c0d43f6c319b9309e1c11f91f0bf201144
Damian <damianchw@proton.me>
2025-11-20 09:51:05 +0100

    small changes

--- model.py
+++ model.py
@@ -70,16 +70,16 @@ class Model:
             ]
 
 
-    def add_user(self, user: User) -> None:
+    def addUser(self, user: User) -> None:
         self.users.append(user)
 
-    def add_item(self, item: Item) -> None:
+    def addItem(self, item: Item) -> None:
         self.items.append(item)
 
-    def clear_users(self) -> None:
+    def clearUsers(self) -> None:
         self.users.clear()
 
-    def clear_items(self) -> None:
+    def clearItems(self) -> None:
         self.items.clear()
 
     def login(self, userName: str, password: str) -> User | None:
