ce2ba531c7bb016dc896f572a0e4b7f8747cced0
David <147860665+Backwards210404@users.noreply.github.com>
2026-01-12 15:35:42 +0100

    Bullshit

--- mainpage.py
+++ mainpage.py
@@ -13,6 +13,9 @@ class MainPage(Page):
     fFilterDropDown: QComboBox
     fFilterInput: QLineEdit
     fFilterButton: QPushButton
+    fStateDropDown: QComboBox
+    fTypePersonDropDown: QComboBox
+    fFilterWidget = QWidget
     def __init__(self):
         super().__init__()
         self.setWindowTitle('Main')
@@ -28,6 +31,8 @@ class MainPage(Page):
         distanceSidePanel = 200
         tableHeaders = ['Gruppe', 'Abteilung', 'Fach', 'Ort', 'Verantworlicher', 'Zustand', 'Löschen']
         filterHeaders = ['Gruppe', 'Abteilung', 'Fach', 'Ort', 'Verantworlicher', 'Zustand']
+        personTypeList = ['Schüler', 'Lehrer', 'Admin']
+        stateList = ['Gebraucht', 'In Reparatur', 'Bestellt', 'Ausgemustert', 'Verliehen', 'Geliefert', 'Geplant', 'Angefordert']
 
         self.fTable = self.createTable(tableHeaders ,x = distanceSidePanel, y = 50, width = 850)
         self.fTable.setEditTriggers(QAbstractItemView.EditTrigger.NoEditTriggers)
@@ -38,36 +43,54 @@ class MainPage(Page):
         )
 
         self.fSidePanel = self.createSidepanel()
-        filterWidget = self.createFilterWidget(filterHeaders)
+        filterWidget = self.createFilterWidget(filterHeaders, personTypeList,  stateList, True)
         filterWidget.move(0, 200)
+        self.fFilterDropDown.currentIndexChanged.connect(self.filterIndexChanged)
 
         self.fHeaderButton.raise_()
         self.fExportButton.raise_()
         self.fAddItemButton.raise_()
         
 
-    def createFilterWidget(self, filterHeaders: list):
+    def createFilterWidget(self, filterHeaders: list, personTypeList: list, stateList: list, isFirstTime: bool):
         filterWidget = QWidget(self)
         vLayout = QVBoxLayout()
 
         filterLabel = self.createText('Suche Nach')
         filterLabel.setMinimumHeight(20)
-
-        self.fFilterDropDown = self.createDropDownMenu(filterHeaders)
-        self.fFilterDropDown.setEditable(True)
-        self.fFilterDropDown.lineEdit().setReadOnly(True)
-        self.fFilterDropDown.lineEdit().setAlignment(Qt.AlignmentFlag.AlignHCenter)
-        self.fFilterDropDown.setMinimumHeight(22)
+        if(isFirstTime):
+            self.fFilterDropDown = self.createDropDownMenu(filterHeaders)
+            self.fFilterDropDown.setEditable(True)
+            self.fFilterDropDown.lineEdit().setReadOnly(True)
+            self.fFilterDropDown.lineEdit().setAlignment(Qt.AlignmentFlag.AlignHCenter)
+            self.fFilterDropDown.setMinimumHeight(22)
 
         self.fFilterButton = self.createButton('Suchen')
         self.fFilterButton.setMinimumHeight(22)
+        filterSelected = self.fFilterDropDown.currentText()
 
-        self.fFilterInput = self.createInput('Bitte gib das Wort hier ein')
-        self.fFilterInput.setAlignment(Qt.AlignmentFlag.AlignHCenter)
-        self.fFilterInput.setMinimumHeight(20)
 
         vLayout.addWidget(filterLabel)
         vLayout.addWidget(self.fFilterDropDown)
+        if (filterSelected == 'Verantworlicher'):
+            self.fTypePersonDropDown = self.createDropDownMenu(personTypeList)
+            self.fTypePersonDropDown.setEditable(True)
+            self.fTypePersonDropDown.lineEdit().setReadOnly(True)
+            self.fTypePersonDropDown.lineEdit().setAlignment(Qt.AlignmentFlag.AlignHCenter)
+            self.fTypePersonDropDown.setMinimumHeight(22)
+            vLayout.addWidget(self.fTypePersonDropDown)
+        elif (filterSelected == 'Zustand'):
+            self.fStateDropDown = self.createDropDownMenu(stateList)
+            self.fStateDropDown.setEditable(True)
+            self.fStateDropDown.lineEdit().setReadOnly(True)
+            self.fStateDropDown.lineEdit().setAlignment(Qt.AlignmentFlag.AlignHCenter)
+            self.fStateDropDown.setMinimumHeight(22)
+            vLayout.addWidget(self.fStateDropDown)
+        else:
+            self.fFilterInput = self.createInput('Bitte gib das Wort hier ein')
+            self.fFilterInput.setAlignment(Qt.AlignmentFlag.AlignHCenter)
+            self.fFilterInput.setMinimumHeight(20)
+            vLayout.addWidget(self.fFilterInput)
         vLayout.addWidget(self.fFilterInput)
         vLayout.addWidget(self.fFilterButton)
         vLayout.setContentsMargins(0,0,0,0)
@@ -77,7 +100,11 @@ class MainPage(Page):
         filterWidget.setFixedSize(200,100)
 
         return filterWidget
-
+    def filterIndexChanged(self):
+        filterHeaders = ['Gruppe', 'Abteilung', 'Fach', 'Ort', 'Verantworlicher', 'Zustand']
+        personTypeList = ['Schüler', 'Lehrer', 'Admin']
+        stateList = ['Gebraucht', 'In Reparatur', 'Bestellt', 'Ausgemustert', 'Verliehen', 'Geliefert', 'Geplant', 'Angefordert']
+        self.fFilterWidget = self.createFilterWidget(filterHeaders, personTypeList,  stateList, False)
 
     def styleSheet(self):
         self.fSidePanel.setStyleSheet('background: Gainsboro')
